3b3e811ae53183877957a6522864c3b868efa851
phobosanomaly <phobos@tux.(none)>
2012-12-10 21:18:11 -0700

    modified:   CMakeLists.txt

---
+++ neo/build/CMakeFiles/idlib.dir/DependInfo.cmake
@@ -0,0 +1,71 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/phobos/doom3/neo/idlib/Base64.cpp" "/home/phobos/doom3/neo/build/CMakeFiles/idlib.dir/idlib/Base64.cpp.o"
+  "/home/phobos/doom3/neo/idlib/BitMsg.cpp" "/home/phobos/doom3/neo/build/CMakeFiles/idlib.dir/idlib/BitMsg.cpp.o"
+  "/home/phobos/doom3/neo/idlib/CmdArgs.cpp" "/home/phobos/doom3/neo/build/CMakeFiles/idlib.dir/idlib/CmdArgs.cpp.o"
+  "/home/phobos/doom3/neo/idlib/Dict.cpp" "/home/phobos/doom3/neo/build/CMakeFiles/idlib.dir/idlib/Dict.cpp.o"
+  "/home/phobos/doom3/neo/idlib/Heap.cpp" "/home/phobos/doom3/neo/build/CMakeFiles/idlib.dir/idlib/Heap.cpp.o"
+  "/home/phobos/doom3/neo/idlib/LangDict.cpp" "/home/phobos/doom3/neo/build/CMakeFiles/idlib.dir/idlib/LangDict.cpp.o"
+  "/home/phobos/doom3/neo/idlib/Lexer.cpp" "/home/phobos/doom3/neo/build/CMakeFiles/idlib.dir/idlib/Lexer.cpp.o"
+  "/home/phobos/doom3/neo/idlib/Lib.cpp" "/home/phobos/doom3/neo/build/CMakeFiles/idlib.dir/idlib/Lib.cpp.o"
+  "/home/phobos/doom3/neo/idlib/MapFile.cpp" "/home/phobos/doom3/neo/build/CMakeFiles/idlib.dir/idlib/MapFile.cpp.o"
+  "/home/phobos/doom3/neo/idlib/Parser.cpp" "/home/phobos/doom3/neo/build/CMakeFiles/idlib.dir/idlib/Parser.cpp.o"
+  "/home/phobos/doom3/neo/idlib/Str.cpp" "/home/phobos/doom3/neo/build/CMakeFiles/idlib.dir/idlib/Str.cpp.o"
+  "/home/phobos/doom3/neo/idlib/Timer.cpp" "/home/phobos/doom3/neo/build/CMakeFiles/idlib.dir/idlib/Timer.cpp.o"
+  "/home/phobos/doom3/neo/idlib/Token.cpp" "/home/phobos/doom3/neo/build/CMakeFiles/idlib.dir/idlib/Token.cpp.o"
+  "/home/phobos/doom3/neo/idlib/bv/Bounds.cpp" "/home/phobos/doom3/neo/build/CMakeFiles/idlib.dir/idlib/bv/Bounds.cpp.o"
+  "/home/phobos/doom3/neo/idlib/bv/Box.cpp" "/home/phobos/doom3/neo/build/CMakeFiles/idlib.dir/idlib/bv/Box.cpp.o"
+  "/home/phobos/doom3/neo/idlib/bv/Frustum.cpp" "/home/phobos/doom3/neo/build/CMakeFiles/idlib.dir/idlib/bv/Frustum.cpp.o"
+  "/home/phobos/doom3/neo/idlib/bv/Sphere.cpp" "/home/phobos/doom3/neo/build/CMakeFiles/idlib.dir/idlib/bv/Sphere.cpp.o"
+  "/home/phobos/doom3/neo/idlib/containers/HashIndex.cpp" "/home/phobos/doom3/neo/build/CMakeFiles/idlib.dir/idlib/containers/HashIndex.cpp.o"
+  "/home/phobos/doom3/neo/idlib/geometry/DrawVert.cpp" "/home/phobos/doom3/neo/build/CMakeFiles/idlib.dir/idlib/geometry/DrawVert.cpp.o"
+  "/home/phobos/doom3/neo/idlib/geometry/JointTransform.cpp" "/home/phobos/doom3/neo/build/CMakeFiles/idlib.dir/idlib/geometry/JointTransform.cpp.o"
+  "/home/phobos/doom3/neo/idlib/geometry/Surface.cpp" "/home/phobos/doom3/neo/build/CMakeFiles/idlib.dir/idlib/geometry/Surface.cpp.o"
+  "/home/phobos/doom3/neo/idlib/geometry/Surface_Patch.cpp" "/home/phobos/doom3/neo/build/CMakeFiles/idlib.dir/idlib/geometry/Surface_Patch.cpp.o"
+  "/home/phobos/doom3/neo/idlib/geometry/Surface_SweptSpline.cpp" "/home/phobos/doom3/neo/build/CMakeFiles/idlib.dir/idlib/geometry/Surface_SweptSpline.cpp.o"
+  "/home/phobos/doom3/neo/idlib/geometry/TraceModel.cpp" "/home/phobos/doom3/neo/build/CMakeFiles/idlib.dir/idlib/geometry/TraceModel.cpp.o"
+  "/home/phobos/doom3/neo/idlib/geometry/Winding.cpp" "/home/phobos/doom3/neo/build/CMakeFiles/idlib.dir/idlib/geometry/Winding.cpp.o"
+  "/home/phobos/doom3/neo/idlib/geometry/Winding2D.cpp" "/home/phobos/doom3/neo/build/CMakeFiles/idlib.dir/idlib/geometry/Winding2D.cpp.o"
+  "/home/phobos/doom3/neo/idlib/hashing/CRC32.cpp" "/home/phobos/doom3/neo/build/CMakeFiles/idlib.dir/idlib/hashing/CRC32.cpp.o"
+  "/home/phobos/doom3/neo/idlib/hashing/MD4.cpp" "/home/phobos/doom3/neo/build/CMakeFiles/idlib.dir/idlib/hashing/MD4.cpp.o"
+  "/home/phobos/doom3/neo/idlib/hashing/MD5.cpp" "/home/phobos/doom3/neo/build/CMakeFiles/idlib.dir/idlib/hashing/MD5.cpp.o"
+  "/home/phobos/doom3/neo/idlib/math/Angles.cpp" "/home/phobos/doom3/neo/build/CMakeFiles/idlib.dir/idlib/math/Angles.cpp.o"
+  "/home/phobos/doom3/neo/idlib/math/Lcp.cpp" "/home/phobos/doom3/neo/build/CMakeFiles/idlib.dir/idlib/math/Lcp.cpp.o"
+  "/home/phobos/doom3/neo/idlib/math/Math.cpp" "/home/phobos/doom3/neo/build/CMakeFiles/idlib.dir/idlib/math/Math.cpp.o"
+  "/home/phobos/doom3/neo/idlib/math/Matrix.cpp" "/home/phobos/doom3/neo/build/CMakeFiles/idlib.dir/idlib/math/Matrix.cpp.o"
+  "/home/phobos/doom3/neo/idlib/math/Ode.cpp" "/home/phobos/doom3/neo/build/CMakeFiles/idlib.dir/idlib/math/Ode.cpp.o"
+  "/home/phobos/doom3/neo/idlib/math/Plane.cpp" "/home/phobos/doom3/neo/build/CMakeFiles/idlib.dir/idlib/math/Plane.cpp.o"
+  "/home/phobos/doom3/neo/idlib/math/Pluecker.cpp" "/home/phobos/doom3/neo/build/CMakeFiles/idlib.dir/idlib/math/Pluecker.cpp.o"
+  "/home/phobos/doom3/neo/idlib/math/Polynomial.cpp" "/home/phobos/doom3/neo/build/CMakeFiles/idlib.dir/idlib/math/Polynomial.cpp.o"
+  "/home/phobos/doom3/neo/idlib/math/Quat.cpp" "/home/phobos/doom3/neo/build/CMakeFiles/idlib.dir/idlib/math/Quat.cpp.o"
+  "/home/phobos/doom3/neo/idlib/math/Rotation.cpp" "/home/phobos/doom3/neo/build/CMakeFiles/idlib.dir/idlib/math/Rotation.cpp.o"
+  "/home/phobos/doom3/neo/idlib/math/Simd.cpp" "/home/phobos/doom3/neo/build/CMakeFiles/idlib.dir/idlib/math/Simd.cpp.o"
+  "/home/phobos/doom3/neo/idlib/math/Simd_3DNow.cpp" "/home/phobos/doom3/neo/build/CMakeFiles/idlib.dir/idlib/math/Simd_3DNow.cpp.o"
+  "/home/phobos/doom3/neo/idlib/math/Simd_AltiVec.cpp" "/home/phobos/doom3/neo/build/CMakeFiles/idlib.dir/idlib/math/Simd_AltiVec.cpp.o"
+  "/home/phobos/doom3/neo/idlib/math/Simd_Generic.cpp" "/home/phobos/doom3/neo/build/CMakeFiles/idlib.dir/idlib/math/Simd_Generic.cpp.o"
+  "/home/phobos/doom3/neo/idlib/math/Simd_MMX.cpp" "/home/phobos/doom3/neo/build/CMakeFiles/idlib.dir/idlib/math/Simd_MMX.cpp.o"
+  "/home/phobos/doom3/neo/idlib/math/Simd_SSE.cpp" "/home/phobos/doom3/neo/build/CMakeFiles/idlib.dir/idlib/math/Simd_SSE.cpp.o"
+  "/home/phobos/doom3/neo/idlib/math/Simd_SSE2.cpp" "/home/phobos/doom3/neo/build/CMakeFiles/idlib.dir/idlib/math/Simd_SSE2.cpp.o"
+  "/home/phobos/doom3/neo/idlib/math/Simd_SSE3.cpp" "/home/phobos/doom3/neo/build/CMakeFiles/idlib.dir/idlib/math/Simd_SSE3.cpp.o"
+  "/home/phobos/doom3/neo/idlib/math/Vector.cpp" "/home/phobos/doom3/neo/build/CMakeFiles/idlib.dir/idlib/math/Vector.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/include/AL"
+  "/usr/include/SDL"
+  "."
+  ".."
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
